f41d22736bc7a71366d7d7f9f4130e60238952bd
IAmSalvaMartini <starraos@gmail.com>
2021-10-31 20:09:32 +0530

    Evolve

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vince.mk
+    $(LOCAL_DIR)/evolution_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vince-user \
-    arrow_vince-userdebug \
-    arrow_vince-eng
+    evolution_vince-user \
+    evolution_vince-userdebug \
+    evolution_vince-eng
--- evolution_vince.mk
+++ evolution_vince.mk
@@ -27,12 +27,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common.mk)
+
+# Build Official
+EVO_BUILD_TYPE := OFFICIAL
+
+# Maintainer Prop
+EVO_DONATE_URL := https://paypal.me/IAmSalvaMartini
+EVO_MAINTAINER := Professor
+
+#gapps
+WITH_GAPPS := true
+PRODUCT_ENFORCE_RRO_TARGETS := *
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := arrow_vince
+PRODUCT_NAME := evolution_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
